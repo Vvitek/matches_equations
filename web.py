c04c7cc52affeff900588cd6a68936754b2cbb6d
s380415 <wr20072@st.amu.edu.pl>
2017-09-12 22:38:09 +0200

    Checking solution done

--- web.py
+++ web.py
@@ -8,11 +8,10 @@ app.secret_key = "Strong enough"
 
 def show_page():
     if request.method == 'POST':
-        jsdata = request.form['javascript_data']
         return("Correct" if(solve(session.get('equation'))==request.form['solution']) else "Incorrect") 
     else:
         session['equation'] = choice(ALL_EQUATIONS)
-        return render_template("home.html", equation = session.get('equation'))
+        return render_template("home.html", equation = session.get('equation'), solution = solve(session.get('equation')))
 
 if __name__ == "__main__":
     app.run(debug = True)
